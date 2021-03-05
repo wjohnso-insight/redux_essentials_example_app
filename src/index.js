d73e0f3b73af1438a88f2c5ed7741725d7e25742
Will <will.johnston@insight.com>
2021-03-05 13:23:05 -0500

    Loading Users
    1. Imported `createAsyncThunk`
    2. Implemented `fetchUsers` async thunk
    3. Handled `fulfilled` response from `fetchUsers`
    4. Called `fetchUsers` at `index.js`

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import './index.css'
 import App from './App'
 import store from './app/store'
 import { Provider } from 'react-redux'
+import { fetchUsers } from './features/users/usersSlice'
 
 import './api/server'
+
+store.dispatch(fetchUsers()) //* Because we only want to call fetchUsers() once, we can call it from index
 
 ReactDOM.render(
   <React.StrictMode>
