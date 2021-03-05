366e0b4b3ef544477511673a0d687be68a835f99
Will <will.johnston@insight.com>
2021-03-05 13:12:54 -0500

    Displaying Loading State
    1. Conditionally render <PostsList> according to state.posts.status

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -4,6 +4,25 @@ import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
 import { ReactionButtons } from './ReactionButtons'
 import { selectAllPosts, fetchPosts } from './postSlice' //* Reusable Selector Function 
+import  { TimeAgo }  from './TimeAgo'
+
+const PostExcerpt = ({ post }) => {
+    return (
+      <article className="post-excerpt" key={post.id}>
+        <h3>{post.title}</h3>
+        <div>
+          <PostAuthor userId={post.user} />
+          <TimeAgo timestamp={post.date} /> 
+        </div>
+        <p className="post-content">{post.content.substring(0, 100)}</p>
+  
+        <ReactionButtons post={post} />
+        <Link to={`/posts/${post.id}`} className="button muted-button">
+          View Post
+        </Link>
+      </article>
+    )
+  }
 
 export const PostsList = () => {
 
@@ -11,6 +30,7 @@ export const PostsList = () => {
     const posts = useSelector(selectAllPosts) //* Reusable Selector (TBH, not sure why selectAllPosts isn't passed `state` object as param?)
     
     const postStatus = useSelector(state => state.posts.status)
+    const error = useSelector(state => state.posts.error)
 
     useEffect(() => {
         if(postStatus === 'idle'){ //* postStatus prevents us from calling dispatch() every time the component renders
@@ -18,24 +38,27 @@ export const PostsList = () => {
         }
     }, [postStatus, dispatch])
 
-    const orderedPosts = posts.slice().sort((a,b) => b.date.localeCompare(a.date));
+    let content
 
-    const renderedPosts = orderedPosts.map(post => (
-        <article className="post-excerpt" key={post.id}>
-            <h3>{post.title}</h3>
-            <h5><PostAuthor userId={post.user} /></h5>
-            {/* //* Displays first 100 characters of post content as a preview*/}
-            <p className="post-content">{post.content.substring(0,100)}</p> 
-            <Link to={`/posts/${post.id}`} className="button muted-button">
-                View Post
-            </Link>
-            <ReactionButtons post={post}/>
-        </article>
-    ))
+    if (postStatus === 'loading'){
+        content = <div className="loader">Loading...</div>
+    }else if (postStatus === 'succeeded'){
+        //* Sort posts in reverse chronological order by datetime string
+        const orderedPosts = posts
+        .slice()
+        .sort((a,b) => b.date.localeCompare(a.date))
+
+        content = orderedPosts.map(post => (
+            <PostExcerpt key={post.id} post={post} />
+       ))
+    }else if (postStatus === 'failed'){
+        content = <div>{error}</div>
+    }
+
     return (
         <section className="posts-list">
             <h2>Posts</h2>
-            {renderedPosts}
+            {content}
         </section> 
     )
 }
